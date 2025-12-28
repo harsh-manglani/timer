31d1f311e3bcae3287cbf7afb0e44724d65fd7a2
Harsh <harshmanglani1234@gmail.com>
2025-12-28 12:20:52 +0530

    added flow and start/stop

--- script.js
+++ script.js
@@ -1,28 +1,67 @@
 const clock = document.getElementById('box');
 const timer = document.getElementById('timer')
+
+const toggleTimer = document.getElementById('toggle-time')
+const toggleFlow = document.getElementById('toggle-flow')
+const up = true
+const down = false
+
+
 let secs = 0
 let mins = 0
 let hrs = 0
 
-setInterval(() => {
-    secs++
-    clock.textContent = secs
-    if (Number.isInteger(secs/60)) {
-        if (hrs>=1) {
-            mins++;
-            timer.textContent = `timer: ${mins}min, ${hrs}hrs`;
-        }
-        else {
-            mins++
-            timer.textContent = `timer: ${mins}min`;
-        }
-}
-    if (Number.isInteger(mins/60) && mins/60) {
-        mins /= 60
-        hrs++
-        timer.textContent = `timer: ${mins}min, ${hrs}hrs`;
-    }
+let toggleStatusTimer = false;
+let toggleStatusFlow = up;
 
-}, 1000)
+toggleFlow.addEventListener('click', () => {
+    if (toggleStatusFlow) {
+        toggleStatusFlow = down;
+        toggleFlow.textContent = 'Flow: Down';
+    } 
+    else {
+        toggleStatusFlow = up;
+        toggleFlow.textContent = 'Flow: Up';
+    }
+})
+
+toggleTimer.addEventListener('click', () => {
+    if (toggleStatusTimer) {
+        toggleStatusTimer = false
+        toggleTimer.textContent = 'Start'
+    }
+    else {
+        toggleStatusTimer = true
+        toggleTimer.textContent = 'Stop'
+    }
+})
+
+setInterval(() => {
+    if (toggleStatusTimer) {
+        if (toggleStatusFlow) {secs++} else {secs--}
+        
+        clock.textContent = secs
+
+        if (Number.isInteger(secs/60) && (secs)) {
+            if (hrs>=1) {
+                if (toggleStatusFlow) {mins++} else {mins--}
+                timer.textContent = `timer: ${mins}min, ${hrs}hrs`;
+            }
+            else {
+                if (toggleStatusFlow) {mins++} else {mins--}
+                timer.textContent = `timer: ${mins}min`;
+            }
+    }
+        if (Number.isInteger(mins/60) && (mins||hrs>0)) {
+            mins /= 60
+            if (toggleStatusFlow) {hrs++} else {hrs--}
+            timer.textContent = `timer: ${mins}min, ${hrs}hrs`;
+}
+
+}
+
+}, 1)
+
+
 
 
