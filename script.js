66caaddbd34543e8b4c362589710d02ab569dc26
Harsh <harshmanglani1234@gmail.com>
2025-12-31 15:03:28 +0530

    added speed

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
+//elements
+
 const clock = document.getElementById('box');
 const timer = document.getElementById('timer')
 
 const toggleTimer = document.getElementById('toggle-time')
 const toggleFlow = document.getElementById('toggle-flow')
+const toggleSpeed = document.getElementById('toggle-speed')
+
+//variables
 const up = true
 const down = false
 
@@ -13,30 +18,12 @@ let hrs = 0
 
 let toggleStatusTimer = false;
 let toggleStatusFlow = up;
+const speeds = ['0.25', '0.50', '0.75', '1', '1.25', '1.50', '2', '2.50', '3'];
+let speedIndex = 3;
 
-toggleFlow.addEventListener('click', () => {
-    if (toggleStatusFlow) {
-        toggleStatusFlow = down;
-        toggleFlow.textContent = 'Flow: Down';
-    } 
-    else {
-        toggleStatusFlow = up;
-        toggleFlow.textContent = 'Flow: Up';
-    }
-})
-
-toggleTimer.addEventListener('click', () => {
-    if (toggleStatusTimer) {
-        toggleStatusTimer = false
-        toggleTimer.textContent = 'Start'
-    }
-    else {
-        toggleStatusTimer = true
-        toggleTimer.textContent = 'Stop'
-    }
-})
-
-setInterval(() => {
+//timer function
+function Timer() {
+    {
     if (toggleStatusTimer) {
         if (toggleStatusFlow) {secs++} else {secs--}
         
@@ -56,12 +43,51 @@ setInterval(() => {
             mins /= 60
             if (toggleStatusFlow) {hrs++} else {hrs--}
             timer.textContent = `timer: ${mins}min, ${hrs}hrs`;
+}}
+}
 }
 
-}
+//timer control
+toggleTimer.addEventListener('click', () => {
+    if (toggleStatusTimer) {
+        toggleStatusTimer = false
+        toggleTimer.textContent = 'Start'
+    }
+    else {
+        toggleStatusTimer = true
+        toggleTimer.textContent = 'Stop'
+    }
+})
 
-}, 1)
+//initial interval
+let intervalId = setInterval(Timer, 1000);
 
+//speed control
+toggleSpeed.addEventListener('click', () => {
+    clearInterval(intervalId)
+    
+    if ((speedIndex)===8) {
+        speedIndex = 0
+        toggleSpeed.textContent = `${speeds[speedIndex]}x`
+    }
+    else {
+        speedIndex++
+        toggleSpeed.textContent = `${speeds[speedIndex]}x`
+    }
+    let speed = 1000 / Number(speeds[speedIndex])
+    intervalId = setInterval(Timer, (speed))
+    console.log(speed)
+})
 
-
+//flow control
+toggleFlow.addEventListener('click', () => {
+    if (toggleStatusFlow) {
+        toggleStatusFlow = down;
+        toggleFlow.textContent = 'Count: Down';
+    } 
+    else {
+        toggleStatusFlow = up;
+        toggleFlow.textContent = 'Count: Up';
+    }
+})
 
